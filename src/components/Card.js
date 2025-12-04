c19ab07fc7a3f44a5fc1cbaac3f9e93d6947f1ae
saiganesh74 <ganeshrejeti1@gmail.com>
2025-12-04 16:57:59 +0530

    Initial scaffold

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import "./Card.css";
+
+export default function Card({ title, description }) {
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
